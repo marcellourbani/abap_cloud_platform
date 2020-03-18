b8a6fa500daf9ee3a1502118eae24ba294ad37c7
Marcello Urbani <marcello.urbani@gmail.com>
2020-03-18 08:39:27 +0000

    endpoint info, password grant

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,24 @@
-test("foo", () => {
-  fail(1)
+// all these tests use an actual CF account defined in setenv.js, will not run without valid credentials
+import { cfPasswordGrant, cfInfo } from "."
+
+const getenv = () => {
+  const { CFUSER, CFPASSWORD, CFENDPOINT } = process.env
+  if (!(CFUSER && CFPASSWORD && CFENDPOINT)) throw "Environment not set"
+  return { CFUSER, CFPASSWORD, CFENDPOINT }
+}
+
+test("cloud foundry endpoint info", async () => {
+  const { CFENDPOINT } = getenv()
+  const info = await cfInfo(CFENDPOINT)
+  expect(info.links?.login?.href).toBeDefined()
+})
+
+test("password grant", async () => {
+  const { CFUSER, CFPASSWORD, CFENDPOINT } = getenv()
+  const info = await cfInfo(CFENDPOINT)
+  const login = info.links?.login?.href
+
+  const token = await cfPasswordGrant(login!, CFUSER, CFPASSWORD)
+  expect(token.accessToken).toBeDefined()
+  expect(token.refreshToken).toBeDefined()
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello,world")
+import ClientOAuth2 from "client-oauth2"
+import got from "got"
+interface CfLink {
+  href: string
+  method?: string
+  meta?: { [key: string]: string }
+}
+const isCfLink = (x: any): x is CfLink => !!x?.href
+export interface CfInfo {
+  guid?: string
+  created_at?: Date
+  updated_at?: Date
+  links: { [key: string]: CfLink | null }
+}
+const isCfInfo = (x: any): x is CfInfo => {
+  let result = !!x.links
+  if (result)
+    for (const k in x.links)
+      if (!(x.links[k] === null || isCfLink(x.links[k]))) {
+        result = false
+        break
+      }
+  return result
+}
+
+export const cfInfo = (cfEndPoint: string) => {
+  const headers = { Accept: "application/json" }
+  return got(cfEndPoint, { headers }).then(resp => {
+    const info = JSON.parse(resp.body)
+    if (!isCfInfo(info)) throw new Error("Failed to retrieve CloufFoundry info")
+    return info
+  })
+}
+
+export function cfPasswordGrant(url: string, user: string, password: string) {
+  const oa = new ClientOAuth2({ accessTokenUri: `${url}/oauth/token` })
+  return oa.owner.getToken(user, password as string, {
+    headers: { Authorization: "Basic Y2Y6" }
+  })
+}
