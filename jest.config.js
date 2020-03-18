72bc0e73ed63e0dac244ce12846057d359989cfc
Marcello Urbani <marcello.urbani@gmail.com>
2020-03-18 08:14:24 +0000

    project setup

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // A set of global variables that need to be available in all test environments
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.json"
+    }
+  },
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: ["ts", "tsx", "js"],
+
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  setupFiles: ["<rootDir>/setenv.js"],
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: ["**/__tests__/*.+(ts|tsx|js)", "**/*.test.ts"],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    "\\.(ts|tsx)$": "ts-jest"
+  },
+  transformIgnorePatterns: ["/node_modules/"]
+}
