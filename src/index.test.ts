3b427cb0ae372a9b66fe0c8c21240edc9186593b
Marcello Urbani <marcello.urbani@gmail.com>
2020-03-18 08:51:24 +0000

    token keys

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 // all these tests use an actual CF account defined in setenv.js, will not run without valid credentials
-import { cfPasswordGrant, cfInfo } from "."
+import { cfPasswordGrant, cfInfo, cfTokenKeys } from "."
 
 const getenv = () => {
   const { CFUSER, CFPASSWORD, CFENDPOINT } = process.env
@@ -11,6 +11,17 @@ test("cloud foundry endpoint info", async () => {
   const { CFENDPOINT } = getenv()
   const info = await cfInfo(CFENDPOINT)
   expect(info.links?.login?.href).toBeDefined()
+})
+
+test("token keys", async () => {
+  const { CFENDPOINT } = getenv()
+  const info = await cfInfo(CFENDPOINT)
+  const login = info.links?.login?.href
+
+  const token = await cfTokenKeys(login!)
+  expect(token.length).toBe(1)
+  expect(token[0].alg).toBeDefined()
+  expect(token[0].value).toBeDefined()
 })
 
 test("password grant", async () => {
--- src/index.ts
+++ src/index.ts
@@ -23,13 +23,34 @@ const isCfInfo = (x: any): x is CfInfo => {
   return result
 }
 
-export const cfInfo = (cfEndPoint: string) => {
+export interface UaaTokenKeys {
+  kid: string // Key ID of key to be used for verification of the token.
+  alg: string // Encryption algorithm
+  value: string // Verifier key
+  kty: string // Key type (RSA)
+  use: string // Public key use parameter - identifies intended use of the public key. (defaults to "sig")
+  n: string // RSA key modulus
+  e: string // RSA key public exponent
+}
+const isUaaTokenKeys = (x: UaaTokenKeys): x is UaaTokenKeys =>
+  !!(x?.kid && x?.value && x?.alg && x?.kty)
+
+export async function cfInfo(cfEndPoint: string) {
   const headers = { Accept: "application/json" }
-  return got(cfEndPoint, { headers }).then(resp => {
-    const info = JSON.parse(resp.body)
-    if (!isCfInfo(info)) throw new Error("Failed to retrieve CloufFoundry info")
-    return info
-  })
+  const resp = await got(cfEndPoint, { headers })
+
+  const info = JSON.parse(resp.body)
+  if (!isCfInfo(info)) throw new Error("Failed to retrieve CloufFoundry info")
+  return info
+}
+
+export async function cfTokenKeys(uaaLoginEndPoint: string) {
+  const headers = { Accept: "application/json" }
+  const resp = await got(`${uaaLoginEndPoint}/token_keys`, { headers })
+  const keys = JSON.parse(resp.body)?.keys
+  if (!Array.isArray(keys) || !keys.every(isUaaTokenKeys))
+    throw new Error("Failed to retrieve token keys")
+  return keys as UaaTokenKeys[]
 }
 
 export function cfPasswordGrant(url: string, user: string, password: string) {
